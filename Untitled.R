adb358a0a1cd86c1312933c6191facb533b7ba88
wgcantah <william.cantah@ucc.edu.gh>
2024-09-17 15:09:38 +0000

    Initial commit

--- Untitled.R
+++ Untitled.R
@@ -2,3 +2,6 @@
 usethis::use_r("cmx")
 devtools::load_all()
 devtools::document()
+usethis::use_git_config(user.name = "wgcantah", user.email = "william.cantah@ucc.edu.gh")
+usethis::use_git()
+usethis::use_github()
